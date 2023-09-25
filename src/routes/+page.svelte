d26fa6fc5431228b5c351d4ded21d4060b13bc10
Aaron So <zaiyou12@gmail.com>
2023-09-25 21:31:33 +0900

    💄 Add cover page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,23 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="bg-gray-900">
+	<div class="relative isolate overflow-hidden">
+		<img
+			src="/main.webp"
+			alt="main iamge of wedding"
+			class="absolute inset-0 -z-10 h-full w-full object-cover"
+		/>
+		<div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56 bg-black bg-opacity-20">
+			<div class="text-left pl-8 sm:pl-4">
+				<p class="text-lg leading-8 text-white">SAVE THE DATE</p>
+				<p class="text-lg leading-8 text-white">JAEWOO AND CHAEYEON</p>
+				<h1 class="font-serif mt-8 text-4xl font-bold tracking-tight text-white sm:mt-12 sm:text-6xl">3</h1>
+				<h1 class="font-serif mt-4 text-4xl font-bold tracking-tight text-white sm:mt-8 sm:text-6xl">DEC</h1>
+				<h1 class="font-serif mt-4 text-4xl font-bold tracking-tight text-white sm:mt-8 sm:text-6xl">2023</h1>
+				<p class="mt-8 text-lg leading-8 text-white sm:mt-12">THE AMBASSADOR SEOUL</p>
+				<p class="text-lg leading-8 text-white">NAMSAN ROOM</p>
+			</div>
+		</div>
+	</div>
+</div>
 
 <style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.gray.100);
-	}
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				serif: ['zolina', 'ui-serif', 'Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif']
+			}
+		}
+	},
+	plugins: []
+};
