941151f5e56ec8c4554a72e28a54c5e531f9e241
Aaron So <zaiyou12@gmail.com>
2023-10-02 14:27:15 +0900

    🎨 Larger file format

---
+++ src/lib/hero.svelte
@@ -0,0 +1,66 @@
+<div class="relative isolate overflow-hidden w-full h-full">
+    <img
+        src="/imgs/main.webp"
+        alt="main iamge of wedding"
+        class="absolute inset-0 -z-10 h-full w-full object-cover"
+    />
+    <div class="wrapper">
+  <div class="items">
+    <p>SAVE THE DATE</p>
+    <p>JAEWOO AND CHAEYEON</p>
+  </div>
+  <div class="items">
+    <h2>3</h2>
+    <h2>DEC</h2>
+    <h2>2023</h2>
+  </div>
+  <div class="items">
+    <p>THE AMBASSADOR SEOUL</p>
+    <p>NAMSAN ROOM</p>
+  </div>
+    </div>
+</div>
+
+<style lang="postcss">
+  .wrapper {
+    display: flex;
+    flex-flow: column;
+    justify-content: space-around;
+    padding-left: 2rem; 
+    max-width: 42rem;
+    height: 100%;
+    margin: auto;
+    text-align: left; 
+    background-color: #00000050; 
+
+    @media (min-width: 640px) { 
+    }
+
+    @media (min-width: 1024px) { 
+    }
+  }
+  p {
+    font-family: 'cantarell', 'ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas';
+    /** text-lg leading-8 text-white */
+    font-size: 1.125rem;
+    line-height: 1.75rem;
+    line-height: 2rem;
+    color: #ffffff;
+  }
+  h2 {
+    font-family: 'zolina', 'ui-serif', 'Georgia', 'Cambria', 'Times New Roman';
+    /** mt-4 text-4xl font-bold tracking-tight text-white sm:mt-8 sm:text-6xl */
+    margin-top: 1rem;
+    font-size: 2.25rem;
+    line-height: 2.5rem;
+    font-weight: 700;
+    letter-spacing: -0.025em;
+    color: #ffffff;
+
+    @media (min-width: 640px) {
+      margin-top: 2rem;
+      font-size: 3.75rem;
+      line-height: 1;
+    }
+  }
+  </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,47 +1,6 @@
-<div class="bg-gray-900">
-	<div class="relative isolate overflow-hidden">
-		<img
-			src="/imgs/main.webp"
-			alt="main iamge of wedding"
-			class="absolute inset-0 -z-10 h-full w-full object-cover"
-		/>
-		<div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56 bg-black bg-opacity-20">
-			<div class="text-left pl-8 sm:pl-4">
-				<p>SAVE THE DATE</p>
-				<p>JAEWOO AND CHAEYEON</p>
-				<h2 class="mt-8">3</h2>
-				<h2>DEC</h2>
-				<h2>2023</h2>
-				<p class="mt-8 sm:mt-12">THE AMBASSADOR SEOUL</p>
-				<p>NAMSAN ROOM</p>
-			</div>
-		</div>
-	</div>
+<script>
+  import Hero from '$lib/hero.svelte';
+</script>
+<div class="bg-gray-200 w-full h-full">
+	<Hero />
 </div>
-
-<style lang="postcss">
-	p {
-		font-family: 'cantarell', 'ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas';
-		/** text-lg leading-8 text-white */
-		font-size: 1.125rem;
-		line-height: 1.75rem;
-		line-height: 2rem;
-		color: #ffffff;
-	}
-	h2 {
-		font-family: 'zolina', 'ui-serif', 'Georgia', 'Cambria', 'Times New Roman';
-		/** mt-4 text-4xl font-bold tracking-tight text-white sm:mt-8 sm:text-6xl */
-		margin-top: 1rem;
-		font-size: 2.25rem;
-		line-height: 2.5rem;
-		font-weight: 700;
-		letter-spacing: -0.025em;
-		color: #ffffff;
-
-		@media (min-width: 640px) {
-			margin-top: 2rem;
-			font-size: 3.75rem;
-			line-height: 1;
-		}
-	}
-</style>
