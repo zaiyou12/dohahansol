1c6bbf0f4d8d61a8640be120becd97aac0925a03
Aaron So <zaiyou12@gmail.com>
2023-11-04 22:19:39 +0900

    🔥 Remove greenCard section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,6 @@
   import Invitation from '$lib/invitation.svelte';
   import GalleryWhite from '$lib/gallery_white.svelte';
   import GalleryBlack from '$lib/gallery_black.svelte';
-  import GreenCard from '$lib/green_card.svelte';
 	import Map from '$lib/map.svelte';
 	import Rsvp from '$lib/rsvp.svelte';
 </script>
