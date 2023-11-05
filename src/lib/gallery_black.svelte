daec5aec894a70b1a0c8d4278a4097d75460f5e9
Aaron So <zaiyou12@gmail.com>
2023-11-05 14:40:57 +0900

    🍱 Update gallery iamges - white gallery - black gallery

--- src/lib/gallery_black.svelte
+++ src/lib/gallery_black.svelte
@@ -1,11 +1,6 @@
 <script>
   import Gallery from '$lib/gallery.svelte';
-
-  const images = [
-    "door_1.webp", "door_2.webp", "door_6.webp", "door_5.webp", "door_7.webp",
-    "house_3.webp", "house_2.webp", "house_6.webp", "house_8.webp", "house_7.webp",
-    "wine_1.webp", "wine_2.webp", "wine_3.webp"
-  ];
+  const images = Array.from(Array(18).keys()).map(x => `black_${(x+1).toString().padStart(2, '0')}.webp`);
 </script>
 
 <div class="h-24 bg-gray-50"></div>
--- src/lib/gallery_white.svelte
+++ src/lib/gallery_white.svelte
@@ -1,25 +1,14 @@
 <script>
   import Gallery from '$lib/gallery.svelte';
-
-  const images = [
-    "white_gallery_cover.webp", "sit_1.jpg", "sit_4.jpg", "sit_5.jpg", "sit_6.jpg",
-    "chair_1.jpg", "chair_2.jpg", "chair_3.jpg", "bed_0.jpg", "bed_1.jpg", "bed_2.jpg", "bed_3.jpg",
-    "bed_4.jpg", "bed_5.jpg", "bed_7.jpg", "bed_9.jpg", "stand.jpg"
-  ];
+  const images = Array.from(Array(18).keys()).map(x => `white_${(x+1).toString().padStart(2, '0')}.webp`);
 </script>
 
 <div class="h-24 bg-gray-50"></div>
 <div class="bg-gray-50 px-6 py-24 sm:pt-32 lg:px-8">
   <div class="text-wrapper">
-    <p class="text-base font-semibold leading-7 tracking-widest gallery">Gallery</p>
+    <p class="sub-title">Gallery</p>
     <h2 class="mt-2">우리의 순간</h2>
   </div>
 </div>
 
 <Gallery imgPath="imgs/white_gallery" {images}/>
-
-<style lang="postcss">
-  p.gallery {
-    color: #328C4B;
-  }
-</style>
