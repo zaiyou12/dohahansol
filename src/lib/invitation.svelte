2c01e6a896dab32613d13f9b76949e6bd025b776
Aaron So <zaiyou12@gmail.com>
2024-03-17 12:21:38 +0900

    ✏️ Update invitation typo

--- src/lib/invitation.svelte
+++ src/lib/invitation.svelte
@@ -9,10 +9,13 @@
 	<div class="text-wrapper">
 		<p class="sub-title">{subTitle}</p>
 		<h2 class="mt-2">{title}</h2>
-		<p class="body mt-10">우리가 함께 영원을 약속할 소중한 그날을 오랫동안 기다려왔습니다.</p>
-		<p class="body">그 오랜 기다림이 새로운 행복으로 시작될 날이 다가옵니다.</p>
-		<p class="body mt-6">바쁘시더라도 부디 참석하시어 저희 둘이 하나 되는 날을</p>
-		<p class="body">함께 축복해 주시고 격려해 주셨으면 좋겠습니다.</p>
+		<p class="body mt-10">우리가 함께 영원을 약속할 소중한 그날을 </p>
+		<p class="body">오랫동안 기다려왔습니다.</p>
+		<p class="body mt-6"> 그 오랜 기다림이 새로운 행복으로</p>
+		<p class="body">시작될 날이 다가옵니다.</p>
+		<p class="body mt-6">바쁘시더라도 부디 참석하시어</p>
+		<p class="body">저희 둘이 하나 되는 날을 함께 축복해 주시고</p>
+		<p class="body">격려해 주셨으면 좋겠습니다.</p>
 	</div>
 </div>
 
